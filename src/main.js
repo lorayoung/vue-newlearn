966004b77392ef8626f6e077cc3a11653b189d0c
吴莉莉 <1424685169@qq.com>
2019-10-09 11:21:56 +0800

    init my project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router.js'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import './assets/lib/mui/css/mui.css'
+Vue.use(MintUI)
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
